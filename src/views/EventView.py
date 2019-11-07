f7a0fc5985714a3b1e5a3c6b11b7dfe6b5a3725d
Diffa <ddiffa2@gmail.com>
2019-11-07 20:07:28 +0700

    update event view

--- src/views/EventView.py
+++ src/views/EventView.py
@@ -1,7 +1,10 @@
 from flask import request, json, Response, Blueprint,g
 from models.EventModel import *
 from shared.Authentication import Auth
-
+from flask import send_from_directory, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import app
 event_api = Blueprint('events',__name__)
 
 event_schema = EventSchema()
@@ -10,15 +13,29 @@ event_schema = EventSchema()
 @event_api.route('/',methods=['POST'])
 @Auth.auth_required
 def create_event():
-    req_data = request.get_json()
-    data,error = event_schema.load(req_data)
+    event = EventModel()
+    event.fk_userid = request.form.get('fk_userid')
+    event.event_name = request.form.get('name')
+    event.event_date = request.form.get('date')
+    event.event_place = request.form.get('place')
+    event.event_detail = request.form.get('detail')
+    event.event_category = request.form.get('category')
+    event.event_talent = request.form.get('talent')
+    event.event_quota = request.form.get('quota')
 
-    if error:
-        return custom_response(error,400)
-    
-    event = EventModel(data)
+    if 'file' not in request.files:
+        flash('No File Part')
+        return redirect(request.url)
+    f = request.files['file']
+
+    if f.filename == '':
+        flash('No selected file')
+    if f:
+        filename = secure_filename(f.filename)
+        f.save(os.path.abspath("../image/"+filename))
+        event.event_image = request.host_url+'image/'+filename
     event.save()
-
+    
     ser_data = event_schema.dump(event).data
 
     return custom_response({'messages':'success','event':ser_data},200)
@@ -65,8 +82,37 @@ def delete(event_id):
 @Auth.auth_required
 def get_event():
     event = EventModel.get_event_by_user(g.user.get('id'))
-    ser_event=event_schema.dump(event).data
+    ser_event=event_schema.dump(event,many=True).data
     return custom_response(ser_event,200)
+
+@event_api.route('/upload/files',methods=['POST'])
+def upload_file():
+    print(request.form)
+    event = EventModel()
+    event.fk_userid = request.form.get('fk_userid')
+    event.event_name = request.form.get('name')
+    event.event_date = request.form.get('date')
+    event.event_place = request.form.get('place')
+    event.event_detail = request.form.get('detail')
+    event.event_category = request.form.get('category')
+    event.event_talent = request.form.get('talent')
+    event.event_quota = request.form.get('quota')
+
+    if 'file' not in request.files:
+        flash('No File Part')
+        return redirect(request.url)
+    f = request.files['file']
+
+    if f.filename == '':
+        flash('No selected file')
+    if f:
+        filename = secure_filename(f.filename)
+        f.save(filename)
+        event.event_image = filename
+    event.save()
+
+    return 'susccess'
+
 
 def custom_response(res,status_code):
     return Response(
