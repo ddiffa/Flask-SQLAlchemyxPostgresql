baada4230a68efbd048f1f0f9b6b54dafed066b8
Diffa <ddiffa2@gmail.com>
2019-11-07 20:06:44 +0700

    add qr blue print

--- src/app.py
+++ src/app.py
@@ -5,19 +5,22 @@ from models import db
 from views.UserView import user_api as user_blueprint
 from views.EventView import event_api as event_blueprint
 from views.TicketView import ticket_api as ticket_blueprint
-
+from views.ParticipantView import participant_api as participant_blueprint
+from views.QrView import qr_api as qr_blueprint
 def create_app(env_name):
-    app = Flask(__name__)
+    app = Flask(__name__,static_folder='image/')
 
+    UPLOAD_FOLDER = 'path/src'
     app.config.from_object(app_config[env_name])
- 
-
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     db.init_app(app)
+
 
     app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
     app.register_blueprint(event_blueprint,url_prefix='/api/v1/events')
     app.register_blueprint(ticket_blueprint,url_prefix='/api/v1/tickets')
-    app.register_blueprint
+    app.register_blueprint(participant_blueprint,url_prefix='/api/v1/participants')
+    app.register_blueprint(qr_blueprint,url_prefix='/api/v1/scanqr')
 
     @app.route('/',methods=['GET'])
     def index():
--- src/config.py
+++ src/config.py
@@ -4,13 +4,15 @@ class Development(object):
     """
     Development environment configuration
     """
+    
     DEBUG = True
     TESTING = False
-    JWT_SECRET_KEY = "hhgaghhgsdhdhdd"
-    SQLALCHEMY_DATABASE_URI = 'postgres://diffa:diffa@localhost/free_event'
-    SQLALCHEMY_TRACK_MODIFICATIONS= False
-    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # JWT_SECRET_KEY="hhgaghhgsdhdhdd"
+    # SQLALCHEMY_DATABASE_URI="postgres://diffa:diffa@localhost/free_event"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    QR_CODE = os.getenv('QR_CODE')
 
 class Production(object):
     """
@@ -18,11 +20,12 @@ class Production(object):
     """
     DEBUG = False
     TESTING = False
-    JWT_SECRET_KEY = "hhgaghhgsdhdhdd"
-    SQLALCHEMY_DATABASE_URI = 'postgres://diffa:diffa@localhost/free_event'
+    # JWT_SECRET_KEY='hhgaghhgsdhdhdd'
+    # SQLALCHEMY_DATABASE_URI='postgres://diffa:diffa@localhost/free_event'
     SQLALCHEMY_TRACK_MODIFICATIONS= False
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    QR_CODE = os.getenv('QR_CODE')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 app_config = {
     'development': Development,
